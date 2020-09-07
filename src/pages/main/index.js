fb830b70ec14d5331e721aba161108f734c5939b
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-09-07 19:26:29 -0300

    [CHG] criação da pagina main

---
+++ src/pages/main/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import api from '../../sevices/api';
+
+export default class Main extends Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
